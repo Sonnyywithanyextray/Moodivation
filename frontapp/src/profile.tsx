c79c1ba69cee2317b758e73af74d9a04c1826bca
Etelson Alcius <etelsonalcius@gmail.com>
2024-10-12 14:40:12 -0400

    I resolved the merge conflicts

--- frontapp/src/profile.tsx
+++ frontapp/src/profile.tsx
@@ -1,23 +1,23 @@
-import React from 'react';
+// import React from 'react';
 
-const styles = {
-  Card: {
-    top: '76px',
-    left: '16px',
-    width: '328px',
-    height: '112px',
-    backgroundColor: '#ffffff',
-    borderRadius: '24px',
-    boxShadow: '0px 4px 6px rgba(0,0,0,0.1)',
-  },
-};
+// const styles = {
+//   Card: {
+//     top: '76px',
+//     left: '16px',
+//     width: '328px',
+//     height: '112px',
+//     backgroundColor: '#ffffff',
+//     borderRadius: '24px',
+//     boxShadow: '0px 4px 6px rgba(0,0,0,0.1)',
+//   },
+// };
 
-const Card = (props) => {
-  return (
-    <div style={styles.Card}>
-      {props.children}
-    </div>
-  );
-};
+// const Card = (props) => {
+//   return (
+//     <div style={styles.Card}>
+//       {props.children}
+//     </div>
+//   );
+// };
 
-export default Card;
+// export default Card;
